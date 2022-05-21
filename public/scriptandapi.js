af05950604bdcf9eb86558372a8e4da9b1d65744
Alexandr_Tabanakov <systema.com2@mail.ru>
2022-05-21 18:04:49 +0300

    Диплом.испарвления js 3

--- public/scriptandapi.js
+++ public/scriptandapi.js
@@ -2,9 +2,26 @@
    const APIsearch = (function() {
    const clientId = 'c7879aa82e414a59b2e762b308012bca';
    const clientSecret = '17d4d73674ec468dbce16c09e9ed2ed4';
+   //cмотрим куки
+   function getCookie(name) {
+      let matches = document.cookie.split(';');
+      for(let i=0;i<matches.length;i++)
+      {
+         if(matches[i][0]===' ')
+        { 
+           matches[i]=matches[i].slice(1);
+        }
+         if(matches[i].substring(0,7)===name)
+         {
+            return matches[i].substring(8);
+         }
+      }
+      return undefined;
+    }
+
    // private methods получения токена
    const _getToken = async () => {
- const cookie=document.cookie;
+   const cookie=getCookie("Mytoken");
       if(!cookie)
       {
       const result = await fetch('https://accounts.spotify.com/api/token', {
@@ -16,7 +33,7 @@
            body: 'grant_type=client_credentials'
        });
        const data = await result.json();
-       document.cookie=`${data.access_token}; max-age=1200`;
+       document.cookie=`Mytoken=${data.access_token}; max-age=${data.expires_in}`;
        return data.access_token;
       }
       else 
@@ -25,33 +42,32 @@
    //поиск
    const _getSearch = async (token,q) => {
       const limit=10;
-      const result = await fetch(`https://api.spotify.com/v1/search?q=${q}&type=track&limit=${limit}`, {
-          method: 'GET',
-          headers: { 'Authorization' : 'Bearer ' + token}
-      });
-      const data = await result.json();
+      const link=`https://api.spotify.com/v1/search?q=${q}&type=track&limit=${limit}`;
+      const data = await ApiReduction(link,token);
       return data;
   }
   //Получаем плейлисты нужной нам категории
   const _getPlaylistByGenre = async (token, categoryId) => {
    const limit = 10;
-   const result = await fetch(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?limit=${limit}`, {
-       method: 'GET',
-       headers: { 'Authorization' : 'Bearer ' + token}
-   });
-
-   const data = await result.json();
+   const link=`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists?limit=${limit}`;
+   const data = await ApiReduction(link,token);
    return data.playlists.items;
 }
 //получаем треки из плейлиста
 const _getTracks = async (token, tracksEndPoint) => {
    const limit=50;
-   const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-       method: 'GET',
-       headers: { 'Authorization' : 'Bearer ' + token}
-   });
-   const data = await result.json();
+   const link=`${tracksEndPoint}?limit=${limit}`;
+   const data = await ApiReduction(link,token);
    return data.items;
+}
+async function ApiReduction(link,token)
+{
+   const result = await fetch(link, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+  });
+  const data = await result.json();
+  return data;
 }
   return {
      getToken(){
@@ -69,71 +85,85 @@ const _getTracks = async (token, tracksEndPoint) => {
   }
 })();
 //функция для вывода треков
-function Music(musicPlayLists,number)
+function Popap(musicPlayLists)
 {
- let message=`<div id="windowlist" class="div"><ol>`
- if(number===0)
- {
+   let message=`<div id="windowlist" class="div">`
+   message+=Music(musicPlayLists);
+   message+=` <button type="button" class="item-actions_type_Close">Закрыть</button>
+   </div>`;
+  return message;
+}
+function Music(musicPlayLists)
+{
     message=`<ol>`;
- }
  for(let i=0;i<musicPlayLists.length;i++)
- {
-   let method=number===0?musicPlayLists[i]:musicPlayLists[i].track;
-   let artists = ""; 
-   method.artists.forEach((artist) => {
-      artists += artist.name+", ";
+  {
+    let method=musicPlayLists[i];
+    let artists = ""; 
+    method.artists.forEach((artist, index) => {
+      artists += artist.name;
+      if (method.artists.length !== index + 1) {
+      artists += ", ";
+      }
       });
-   artists=artists.slice(0,-2);
-   //Бывает что нет фотографии
-   if(method.album.images.length<2)
-   continue; 
-   //images[2] - это размер нашей картинки
-   const picture=method.album.images[2].url;
-   const track=method.name;  
-   const preview=method.preview_url;
-   if(preview===null)
+   //cмотрим сколько есть фотографий у данного альбома
+    //Бывает что нет фотографии
+    if(method.album.images.length<2)
+     {  
+        continue; 
+     } 
+    //images[2] - это размер нашей картинки
+    const picture=method.album.images[2].url;
+    const track=method.name;  
+    const preview=method.preview_url;
+    if(preview===null)
       continue;
    message+=`<li class="li"><img src="${picture}" alt=""> ${track} <br><p class="artists">${artists} </p> <audio controls>          
           <source src="${preview}" type="audio/ogg; codecs=vorbis"> 
           </audio></li>`;
  }
- message+=number===0?`</ol>`:`</ol>
- <button type="button" class="item-actions_type_Close">Закрыть</button>
- </div>`;
+   message+=`</ol>`;
   return message;
 }
-
-const APPController = (function(APICtrl){
-const $playList=document.querySelector('.music');
-//метод проверки для понятия на какой странице мы находимся
-if($playList!=null)
+function closePopup(event)
+{  
+   const closePop=(element)=>{
+      element.closest('.div').remove();
+    }
+    if(event.target.classList.contains('item-actions_type_Close'))
+    {
+        const lie=event.target.closest('.div');
+        closePop(event.target);
+    }
+}
+async function albomList(button)
 {
-   //мы работаем с search.html
+   const token= await APIsearch.getToken();  
+   //записываю айди плейлиста и его порядковый номер
+  let numberplaylist=button.id.split(' '); 
+   const play=await APIsearch.getPlaylistByGenre(token,numberplaylist[0]);
+   const musicPlayLists=await APIsearch.getTracks(token,play[numberplaylist[1]].tracks.href);
+   musicPlayLists.forEach((element,index) => {
+     musicPlayLists[index]=musicPlayLists[index].track;
+  });
+  return musicPlayLists;
+}
+//работа с searc.hthml
+async function searchhtml(PLAY_LIST)
+{
    //клик по кнопке(плейлисту)
-   const $searchInput=document.querySelector('.header__search');
-   ($playList.querySelectorAll('.content__button')).forEach((button)=>{
+   const SEARCH_INPUT=document.querySelector('.header__search');
+   PLAY_LIST.querySelectorAll('.content__button').forEach((button)=>{
    button.addEventListener('click',async(event)=>
-   { 
-       const token= await APICtrl.getToken();  
-       const play=await APICtrl.getPlaylistByGenre(token,button.id);
-       const musicPlayLists=await APICtrl.getTracks(token,play[0].tracks.href);
-      $playList.insertAdjacentHTML('beforeend',Music(musicPlayLists));
+   {  
+      PLAY_LIST.insertAdjacentHTML('beforeend',Popap(await albomList(button)));
       return;
    })
 })
-//закрыть всплывающее окно
-const closePopup=(element)=>{
-   element.closest('.div').remove();
- }
-$playList.addEventListener('click',(event)=>{
-   if(event.target.classList.contains('item-actions_type_Close'))
-   {
-       const lie=event.target.closest('.div');
-       closePopup(event.target);
-   }
-   return;
+  //закрыть всплывающее окно
+  PLAY_LIST.addEventListener('click',(event)=>{
+    closePopup(event);
  }) 
- 
  const debounce=(fn,ms)=>{
     let timeout;
     return function(){
@@ -148,69 +178,49 @@ $playList.addEventListener('click',(event)=>{
    const searchValue = e.target.value;
    //вывести все, если символов введено <2
    if (searchValue.length < 2) {
-     $playList.querySelector('ol').remove(); 
-     $playList.insertAdjacentHTML('beforeend','<ol></ol>');
+     PLAY_LIST.querySelector('ol').remove(); 
+     PLAY_LIST.insertAdjacentHTML('beforeend','<ol></ol>');
      //для проверки на наличие скрытых объектов
-      const check=$playList.querySelector('.hide');
+      const check=PLAY_LIST.querySelector('.hide');
       if(check!=null)
       check.classList.remove('hide');
        return;
    }
-  $playList.querySelector('.content__img').classList.add('hide');
+  PLAY_LIST.querySelector('.content__img').classList.add('hide');
    //токены
-   const token= await APICtrl.getToken();  
-   const typemusic=await APICtrl.getSearch(token,searchValue);
+   const token= await APIsearch.getToken();  
+   const typemusic=await APIsearch.getSearch(token,searchValue);
    //удаляю, потому что будем заводить новый тег
-   $playList.querySelector('ol').remove(); 
+   PLAY_LIST.querySelector('ol').remove(); 
    //вывод песен по названию
    const musicPlayLists=typemusic.tracks.items;
-    $playList.insertAdjacentHTML('beforeend', Music(musicPlayLists,0));
+    PLAY_LIST.insertAdjacentHTML('beforeend', Music(musicPlayLists));
 }
 //дебаунс функция вызванная с задержкой
   event=debounce(event,250);
+SEARCH_INPUT.addEventListener('input',event);
+}
 
-$searchInput.addEventListener('input',event);
+const APPController = (function(){
+const PLAY_LIST=document.querySelector('.music');
+//метод проверки для понятия на какой странице мы находимся
+if(PLAY_LIST!=null)
+{
+   //метод который вызывается на search.html странице
+     searchhtml(PLAY_LIST);
 }
 else
 {
-   //используем другую страницу, в данном случае glavnaya.html
-   const $playListGlavnaya=document.querySelector('.playlistsgl');
-   Array.from($playListGlavnaya.querySelectorAll('.button_home')).forEach((button)=>{
+   //используем другую страницу, в данном случае index.html
+   const PLAY_LISTGlavnaya=document.querySelector('.playlistsgl');
+     PLAY_LISTGlavnaya.querySelectorAll('.button_home').forEach((button)=>{
       button.addEventListener('click',async(event)=>
       { 
-         const token= await APICtrl.getToken();  
-         let buttonid="";
-         let numberplaylist="";
-         //записываю айди плейлиста и его порядковый номер
-         let flag=0;
-         for(let i=0;i<button.id.length;i++)
-         {
-            if(flag===0)
-            {
-               if(button.id[i]==" ")
-               {
-                  flag=1;
-                  continue;
-               }
-              buttonid+=button.id[i];
-            }
-            else
-            numberplaylist+=button.id[i];
-         }
-          const play=await APICtrl.getPlaylistByGenre(token,buttonid);
-          const musicPlayLists=await APICtrl.getTracks(token,play[numberplaylist].tracks.href);
-         $playListGlavnaya.insertAdjacentHTML('beforeend',Music(musicPlayLists));
+         PLAY_LISTGlavnaya.insertAdjacentHTML('beforeend',Popap(await albomList(button))); 
       })
    })
-   const closePopup=(element)=>{
-      element.closest('.div').remove();
-    }
-   $playListGlavnaya.addEventListener('click',(event)=>{
-      if(event.target.classList.contains('item-actions_type_Close'))
-      {
-          const lie=event.target.closest('.div');
-          closePopup(event.target);
-      }
+   PLAY_LISTGlavnaya.addEventListener('click',(event)=>{
+      closePopup(event);
     })    
 }
-})(APIsearch);
+})();
